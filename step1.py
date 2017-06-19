bcacec1c5d6611281b4f5705cffc86a72a7fb5e5
Gijs Weterings <gijsweterings+github@gmail.com>
2017-06-19 14:15:47 +0200

    Self-similarity matrix works

--- step1.py
+++ step1.py
@@ -2,32 +2,38 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-#cap = cv2.VideoCapture("House.Of.Cards.S01E01.720p.BluRay.x265.mp4")
-cap = cv2.VideoCapture("House.Of.Cards.S01E02.720p.BluRay.x265.mp4") 
+DEBUG = True
 
-totalhist = np.array([np.ones(256)], dtype="f").T
+cap = cv2.VideoCapture("videos/House.Of.Cards.S01E01.720p.BluRay.x265.mp4") 
+
+totalhist = np.array([np.ones(256)], np.float64).T
 for i in range(0, 7200): #5 minutes of footage, based on 24 fps
     retrieved, colorimg = cap.read()
     image = cv2.cvtColor(colorimg, cv2.COLOR_BGR2GRAY)
+    hsv = cv2.cvtColor(colorimg, cv2.COLOR_BGR2HSV)
+
     if(retrieved):
-        hist = cv2.calcHist([image], [0], None, [256], [0,256])
-        hist = (hist / sum(hist))
-#        print(hist.shape)
-#        print(totalhist.shape)
+        hist = cv2.calcHist([colorimg], [0], None, [256], [0, 256])
+        hist2 = cv2.calcHist([colorimg], [1], None, [256], [0, 256])
+        hist3 = cv2.calcHist([colorimg], [2], None, [256], [0, 256])
+        # hist = cv2.calcHist([image], [0], None, [256], [0,256])
+        # hist = (hist / sum(hist)) * 256 * 1
+        if(DEBUG and i % 1000 == 0):
+            print("frame dimensions: ", colorimg.shape)
+            print("histogram shape: ", hist.shape)
+            print("histogram shape: ", hist2.shape)
+            print("histogram shape: ", hist3.shape)
+            print("totalHist shape: ", totalhist.shape)
+            # cv2.imshow("a", hsv)
+            # cv2.waitKey(0)
+            plt.plot(hist)
+            plt.show()
         totalhist = np.concatenate([totalhist, hist], axis=1)
         
 # Remove initial column
 totalhist = np.delete(totalhist, 0, axis=1)
 print(totalhist.shape)
-cv2.imwrite('ep2.png', totalhist)
+cv2.imwrite('ep1.png', totalhist)
 cv2.waitKey(0)
-
-# while(True):
-#x, y = cap.read()
-#print(y)
-# cv2.imshow('', y)
-#cv2.imshow('output.png', y)
-#cv2.waitKey(0)
-
 
 cap.release()
--- step2.py
+++ step2.py
@@ -1,28 +1,28 @@
 import cv2
 import numpy as np
+import math
+from scipy import spatial
 from multiprocessing import Pool
 
-ep1 = cv2.imread("ep1.png")
-ep2 = cv2.imread("ep2.png")
+ep1 = np.array(cv2.imread("ep1.png"))
+ep2 = np.array(cv2.imread("ep2.png"))
+
+def cosine_similarity(x,y):
+    a, b = x.astype(float), y.astype(float)
+    return (1 - spatial.distance.cosine(a,b)) * 256
 
 def inside(i):
     arr = np.zeros((7200,1), np.float64)
     for j in range(7200):
-        arr[j,0] = cosineSim(ep1[:,i,0],ep2[:,j,0])
+        arr[j,0] = cosine_similarity(ep1[:,i,0],ep2[:,j,0])
     return arr
-
-def cosineSim(v1, v2):
-    return np.dot(v1, v2) / (len(v1) * len(v2))
 
 pool = Pool(8)
 
-img = np.zeros((7200, 7200, 1), np.float64)
-
 rows = pool.map(inside, range(7200))
 
-
 result = np.array(rows)
-print(result.shape)
 cv2.imwrite('similaritymatrix.png', result)
-cv2.waitKey(0)
 pool.close()
+
+
