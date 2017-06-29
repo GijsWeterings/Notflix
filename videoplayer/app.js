5dc7bd461b2e5b0e5af202f55dd0a459f56d379f
Gijs Weterings <gijsweterings+github@gmail.com>
2017-06-29 12:05:59 +0200

    Add skip intro button and highlight in progress bar

--- videoplayer/app.js
+++ videoplayer/app.js
@@ -1,7 +1,14 @@
 const episodesList = document.querySelector("#episode-list");
 const videoPlayer = document.querySelector("#player");
 const subtitles = document.querySelector("#subtitles");
+const skipButton = document.querySelector("#skipIntro");
+const progressIndicator = document.querySelector("#progressIndicator");
 let episodes = {};
+
+let metadata = {};
+let duration = 0;
+
+loadEpisodeInfo("/ep1.json");
 
 fetch("/episodes.json")
   .then(async res => {
@@ -14,8 +21,66 @@ fetch("/episodes.json")
     episodeNodes.forEach(element => episodesList.appendChild(element), this);
   });
 
-videoPlayer.setAttribute("src", "videos/House.Of.Cards.S01E01.720p.BluRay.x265.mp4");
-subtitles.setAttribute("src", "videos/subtitles/HOC.S01E01.vtt");
+videoPlayer.addEventListener("loadedmetadata", videoMetaData => {
+  const videoDuration = document.querySelector("#videoDuration");
+
+  duration = videoMetaData.srcElement.duration;
+  videoDuration.innerHTML = formatSeconds(duration);
+  addProgressBarHighlight(metadata.intro, duration);
+});
+
+videoPlayer.addEventListener("timeupdate", event => {
+  const currentTime = document.querySelector("#currentTime");
+
+  const timestamp = convertMsToSeconds(event.timeStamp);
+  currentTime.innerHTML = formatSeconds(timestamp);
+  const deltaToStart = timestamp - parseInt(metadata.intro.start);
+  const deltaToEnd = timestamp - parseInt(metadata.intro.end);
+  if (deltaToStart > 0 && deltaToStart < 5) {
+    skipButton.style.opacity = 1;
+  }
+  else if (deltaToEnd > 0 && deltaToEnd < 5) {
+    skipButton.style.opacity = 0;
+  }
+
+  progressIndicator.style.left = String(100 * timestamp / videoPlayer.duration).replace(",", ".") + "%";
+  console.log(String(100 * timestamp / videoPlayer.duration) + "%");
+
+});
+
+skipButton.addEventListener("click", () => {
+  videoPlayer.currentTime = parseInt(metadata.intro.end);
+  skipButton.style.opacity = 0;
+});
+
+function loadEpisodeInfo(episodeJSONPath) {
+  fetch(episodeJSONPath)
+    .then(async res => {
+      const episodeInfo = await res.json();
+
+      videoPlayer.setAttribute("src", episodeInfo.src);
+      subtitles.setAttribute("src", episodeInfo.subtitles);
+      metadata = episodeInfo;
+    });
+}
+
+function addProgressBarHighlight(highlightTimestamps, totalDuration) {
+  const progressBarHighlights = document.querySelector("#progress-bar-highlights");
+  const highlight = document.createElement("span");
+  highlight.style.width = String(Math.round(((parseInt(highlightTimestamps.end) - parseInt(highlightTimestamps.start)) / totalDuration) * 100)) + "%";
+  highlight.style.left = String(Math.round(100 * parseInt(highlightTimestamps.start) / totalDuration)) + "%";
+  progressBarHighlights.appendChild(highlight);
+}
+
+function convertMsToSeconds(timeInMs) {
+  return Math.floor(timeInMs / 1000);
+}
+
+function formatSeconds(timeInSec) {
+  const date = new Date(null);
+  date.setSeconds(timeInSec);
+  return date.toISOString().substr(11, 8);
+}
 
 function playPause() {
   if(videoPlayer.paused || videoPlayer.ended)
--- videoplayer/server.js
+++ videoplayer/server.js
@@ -3,6 +3,7 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app.use(express.static(__dirname));
+app.use(express.static("/home/gijs/Projects/TUDelft/Notflix/")); // TODO Fix
 
 // app.get("/", (req, res) => {
 //   res.sendfile("index.html");
