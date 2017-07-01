bcd94bec6cc24da6cba53042313eea5c343a0293
Gijs Weterings <gijsweterings+github@gmail.com>
2017-07-02 00:38:02 +0200

    cleanup

--- scene_histogram.py
+++ scene_histogram.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-#docker run -v /c/Users/Laura/Notflix:/videos -it opencv3
 import numpy as np
 import cv2
 import json
@@ -22,21 +21,22 @@ def extract_frame_hsv_histogram(frame):
         [channel_bins],
         channel_range
         )   
-  histogram = histogram / np.sum(histogram)  # Return a normalized histogram.  
-  return histogram.flatten('C')
+  # Return a normalized histogram.  
+  histogram = histogram / np.sum(histogram)
+  return histogram.flatten("C")
 
 def hsv_histograms_similarity(hs_histogram0, hs_histogram1):
   similarity = cv2.compareHist(hs_histogram0, hs_histogram1, cv2.HISTCMP_INTERSECT )  
   return similarity
 
-cap = cv2.VideoCapture('house2.mp4')
+cap = cv2.VideoCapture("house2.mp4")
 
 time = 0
 scene = 1
 length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 timestamps = []
 while(time < length): 
-    time = time+1
+    time = time + 1
     ret, frame = cap.read()
     ret2, frame2 = cap.read()
     if ret == False or ret2 == False:
@@ -47,8 +47,8 @@ while(time < length):
     # If similarity is lower than 0.75 possibly a scene change
     if sim < 0.75:
         if scene > 1:
-            lastframe1 = cv2.imread("firstframe"+str(scene-1)+".png")
-            lastframe2 = cv2.imread("secondframe"+str(scene-1)+".png")
+            lastframe1 = cv2.imread("firstframe" + str(scene - 1) + ".png")
+            lastframe2 = cv2.imread("secondframe" + str(scene - 1) + ".png")
             histogramlast1 = extract_frame_hsv_histogram(lastframe1)
             histogramlast2 = extract_frame_hsv_histogram(lastframe2)
             #sim1 = hsv_histograms_similarity(histogramlast1, histogram1)
@@ -64,21 +64,21 @@ while(time < length):
             if sim2 > 0.8 or sim3 > 0.8 or sim4 > 0.8:
                 continue
             
-        cv2.imwrite("firstframe"+str(scene)+".png", frame)
-        cv2.imwrite("secondframe"+str(scene)+".png", frame2)
-        second = (float(time*2) / 24.0)
+        cv2.imwrite("firstframe" + str(scene) + ".png", frame)
+        cv2.imwrite("secondframe" + str(scene) + ".png", frame2)
+        second = float(time * 2) / 24.0
         timestamps.append(round(second))
-        second = float(second)/60.0
-        m,s = divmod(second, 1)
-        s = s*60.0
+        second = float(second) / 60.0
+        m, s = divmod(second, 1)
+        s = s * 60.0
         print ("Scene " + str(scene))
         # This is the moment in mm:ss in the video
-        print ("Min " + str(int(second)) +" Sec "+ str(int(s)))
-        print ("seconds "+str(second*60))
-        print ("Sim "+str(sim))
-        print (" ")
-        scene = scene+1
+        print ("Min " + str(int(second)) + " Sec " + str(int(s)))
+        print ("seconds " + str(second * 60))
+        print ("Sim " + str(sim) + "\n")
+
+        scene = scene + 1
         print (timestamps[:])
 
-with open("list_timestamps_scenes", 'wb') as outfile:
+with open("list_timestamps_scenes", "wb") as outfile:
     json.dump(timestamps, outfile)
